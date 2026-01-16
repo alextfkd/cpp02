dfc23919855060e1cad027121e3b7a15b56d641f
tkatsuma <tkatsuma@student.42.fr>
2026-01-16 17:23:14 +0900

    add test

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tkatsuma <tkatsuma@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/18 15:50:15 by tkatsuma          #+#    #+#             */
-/*   Updated: 2025/12/26 15:48:48 by tkatsuma         ###   ########.fr       */
+/*   Updated: 2026/01/16 17:21:57 by tkatsuma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 #include "Fixed.hpp"
 
 int main(void) {
+  std::cout << "Subject test start." << std::endl;
   Fixed a;     // NOLINT
   Fixed b(a);  // NOLINT
   Fixed c;     // NOLINT
@@ -22,5 +23,17 @@ int main(void) {
   std::cout << a.getRawBits() << std::endl;
   std::cout << b.getRawBits() << std::endl;
   std::cout << c.getRawBits() << std::endl;
+  std::cout << "Subject test finished." << std::endl;
+  std::cout << "setRawBits() -> getRawBits() test start." << std::endl;
+  std::cout << "Fixed 'a' address: " << &a << std::endl;
+  std::cout << "Fixed 'b' address: " << &b << std::endl;
+  std::cout << "Fixed 'c' address: " << &c << std::endl;
+  a.setRawBits(1);
+  b.setRawBits(2);
+  c.setRawBits(3);
+  std::cout << a.getRawBits() << std::endl;
+  std::cout << b.getRawBits() << std::endl;
+  std::cout << c.getRawBits() << std::endl;
+  std::cout << "setRawBits() -> getRawBits() test finished." << std::endl;
   return 0;
 }
